830d2d1f5070512c6169da2d5cfe019cc8f4c790
Niklas Elsbrock <mail@nelsbrock.de>
2023-02-19 01:08:02 +0100

    major update, bump version to 0.3.0

--- src/bin/mclanfake.rs
+++ src/bin/mclanfake.rs
@@ -1,43 +1,85 @@
+use clap::builder::{NonEmptyStringValueParser, TypedValueParser};
+use std::ffi::OsString;
 use std::net::SocketAddrV4;
-use std::process;
-
-use clap::Clap;
+use std::process::ExitCode;
+use std::string::ToString;
 use std::time::Duration;
 
-/// Fakes discovery messages of LAN Minecraft servers.
-#[derive(Clap)]
-#[clap(version = "0.2.0", author = "Niklas Elsbrock <mail@nelsbrock.de>")]
-struct Opts {
-    /// "Message of the day" to use. This supports Minecraft formatting codes.
-    motd: String,
+use clap::Parser;
+use mclanfake::Repeat;
+use os_str_bytes::OsStrBytes;
 
-    /// The port to advertise.
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// Advertise this "message of the day" to clients.
     ///
-    /// This does not need to be a valid port or number. It supports Minecraft formatting codes.
-    #[clap(short = 'p', long, default_value = "0")]
-    advertise_port: String,
+    /// This supports Minecraft formatting codes.
+    #[arg(
+        short,
+        long,
+        value_parser,
+        value_name = "STRING",
+        default_value = "A Minecraft Server"
+    )]
+    motd: OsString,
 
-    /// The bind address to use for the UDP socket.
-    #[clap(long, default_value = "0.0.0.0:34524")]
-    udp_bind: SocketAddrV4,
+    /// Advertise this "port" to clients.
+    ///
+    /// This does not need to be a valid port or number,
+    /// and Minecraft formatting codes are supported.
+    #[arg(
+        short,
+        long,
+        value_parser,
+        value_name = "STRING",
+        default_value = "25565"
+    )]
+    port: OsString,
 
-    /// The amount of seconds to wait after each send operation.
-    #[clap(long, default_value = "1.5")]
-    interval: f64,
+    /// Only send one advertisement message, then exit.
+    #[arg(long, conflicts_with = "interval")]
+    once: bool,
+
+    /// Wait this number of seconds after sending each advertisement message.
+    #[arg(
+        long,
+        value_name = "SECONDS",
+        value_parser = NonEmptyStringValueParser::new()
+            .try_map(
+                |s| s.parse::<f64>().map_err(|e| e.to_string())
+                    .and_then(|f| Duration::try_from_secs_f64(f).map_err(|e| e.to_string()))
+            ),
+        default_value = "1.5"
+    )]
+    interval: Duration,
+
+    /// Bind the UDP socket to this address.
+    #[arg(long, value_name = "ADDRESS", default_value = "0.0.0.0:0")]
+    bind: SocketAddrV4,
 }
 
-fn main() {
-    let opts: Opts = Opts::parse();
-    match mclanfake::run(
-        opts.udp_bind,
-        &opts.motd,
-        &opts.advertise_port,
-        Duration::from_secs_f64(opts.interval),
-    ) {
-        Ok(_) => unreachable!(),
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            process::exit(1);
+fn main() -> ExitCode {
+    let args: Args = Args::parse();
+
+    let repeat = if args.once {
+        Repeat::Once
+    } else {
+        Repeat::Forever {
+            interval: args.interval,
         }
     };
+
+    match mclanfake::run(
+        args.bind,
+        &args.motd.to_raw_bytes(),
+        &args.port.to_raw_bytes(),
+        repeat,
+    ) {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("error: {err}");
+            ExitCode::FAILURE
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,38 @@
+use format_bytes::format_bytes;
 use std::net::{SocketAddrV4, UdpSocket};
 use std::time::Duration;
 use std::{io, thread};
 
-const MULTICAST_ADDR: &str = "224.0.2.60:4445";
+const ANNOUNCEMENT_ADDR: &str = "224.0.2.60:4445";
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+pub enum Repeat {
+    Once,
+    Forever { interval: Duration },
+}
+
+fn construct_message(motd: &[u8], advertise_port: &[u8]) -> Vec<u8> {
+    format_bytes!(b"[MOTD]{}[/MOTD][AD]{}[/AD]", motd, advertise_port)
+}
 
 pub fn run(
     bind_addr: SocketAddrV4,
-    motd: &str,
-    advertise_port: &str,
-    sleep: Duration,
+    motd: &[u8],
+    advertise_port: &[u8],
+    repeat: Repeat,
 ) -> io::Result<()> {
     let socket = UdpSocket::bind(bind_addr)?;
-    socket.connect(MULTICAST_ADDR)?;
+    socket.connect(ANNOUNCEMENT_ADDR)?;
     let buf = construct_message(motd, advertise_port);
-    loop {
-        socket.send(&buf)?;
-        thread::sleep(sleep);
-    }
-}
 
-fn construct_message(motd: &str, advertise_port: &str) -> Vec<u8> {
-    format!("[MOTD]{}[/MOTD][AD]{}[/AD]", motd, advertise_port).into_bytes()
+    match repeat {
+        Repeat::Once => socket.send(&buf).map(|_| ()),
+        Repeat::Forever { interval } => loop {
+            let result = socket.send(&buf);
+            if let Err(err) = result {
+                eprintln!("error: {err}");
+            }
+            thread::sleep(interval);
+        },
+    }
 }
